f330d313b36f63a4ba4ab2c4119e43497eeb78fe
jppi <jppi@aqua.dtu.dk>
2021-02-16 18:10:15 +0100

    Added flux calculation for model 3

--- Model3.m
+++ Model3.m
@@ -30,15 +30,61 @@ S = .5; % [-] level of defense adopted by the ciliates
 options = odeset('NonNegative',[1 2 3]);
 [t, C] = ode15s(@(t,C) dCdt(C,t,S), 0:10:1000, [1 .1 .1],options);
 
-figure, 
-subplot(311)
-plot(t,C(:,1))
-ylabel('Diatoms')
+% figure, 
+% subplot(311)
+% plot(t,C(:,1))
+% ylabel('Diatoms')
+% hold on
+% 
+% subplot(312)
+% plot(t,C(:,2))
+% ylabel('Ciliates')
+% hold on
+% 
+% subplot(313)
+% plot(t,C(:,3))
+% ylabel('Copepods')
+% hold on
 
-subplot(312)
-plot(t,C(:,2))
-ylabel('Ciliates')
+%% Compute detritus creation rate
+phi = 0.4; % [-] (1-assim efficiency)/assim efficiency - ratio between growth and what is excreted as FP
+R = C(end,1); %  [gC m^-3] equilibrium concentration of diatoms
+N = C(end,2); %  [gC m^-3] equilibrium concentration of ciliates
+P = C(end,3); %  [gC m^-3] equilibrium concentration of copepods
 
-subplot(313)
-plot(t,C(:,3))
-ylabel('Copepods')
+
+Crea_R = 0; %phi*r*(1-C(1)/K)*C(1) %assume only leakage from diatoms
+Crea_N = (1-epsN)*fcost1(tau,S)*bN*N*R/wN; % [gC m^-3 day^-1] Creation rate of N fecal pellets
+Crea_P = (1-epsPR)*bPR*R*P/wP + (1-epsPN)*fgain(tau,S)*bPN*N*P/wP; % [gC m^-3 day^-1] Creation rate of P fecal pellets
+
+Crea_Rc = muR*R; % [gC m^-3 day^-1] Creation rate of carcasses from R
+Crea_Nc = muN*N; % [gC m^-3 day^-1] Creation rate of carcasses from N
+Crea_Pc = muP*P; % [gC m^-3 day^-1] Creation rate of carcasses from P
+
+%% Now plot the flux
+mld = 10; % [m] Mixed layer depth
+alpha = @(z) 0.5; % [day^-1] Degradation rate - assumed a Q10 of 2 and a reference temperature of 18
+
+uRfp = 10^-5; % [m/day] Not used, sinking rate of R fecal pellets
+uNfp = 0.1; % [m/day] Sinking rate of N fecal pellet
+uPfp = 50; % [m/day] Sinking rate of P fecal pellet
+uRc = 1; % [m/day] Sinking rate of R carcasses
+uNc = 10; % [m/day] Sinking rate of N carcasses
+uPc = 500; % [m/day] Sinking rate of P carcasses
+speed = [uRfp uNfp uPfp uRc uNc uPc]'; % [m/day] all speeds
+
+Zwc = 0:5:500;
+Flux = zeros(size(Zwc,2),1);
+
+So = @(z) (z<mld)*[Crea_R Crea_N Crea_P Crea_Rc Crea_Nc Crea_Pc]';
+[z, C] = ode45(@(z,c) (So(z)- alpha(z)*c)./speed,Zwc, [0 0 0 0 0 0]); % [C is gC / m^3 concentration of detritus in the water column]
+
+
+F = speed'.*C; % [gC / m2 / day] Flux at each depth
+Ftot = sum(F,2); % [gC / m2 / day] Total flux at each depth
+
+% figure,
+plot(Ftot, Zwc)
+ylabel('Depth [m]')
+xlabel('Flux [gC m^{-2} day^{-1}]')
+set(gca,'ydir','reverse')
